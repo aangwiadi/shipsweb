bee0e7c2bff360d7e92c1e47f2dee872d9470722
ghovmand <ghovmand@1fe88100-d82c-11de-a027-019ae363d039>
2009-12-01 08:49:58 +0000

    Css cleanup

--- trunk/system/application/views/login/login_view.php
+++ trunk/system/application/views/login/login_view.php
@@ -1,15 +1,19 @@
 <div id="login_form">
   <h1>Login</h1>
-  
-  <?php echo form_open('login/validate');?></label>
-  <label class="width1 first">Username<?php echo form_input(array('name' => 'username',
-									      'value' => '',
-									      'class' => 'text',
-									      'type' => 'text'));?></label>
-  <label class="width1 first">Password<?php echo form_password(array('name' => 'password',
-									  'value' => '',
-									  'class' => 'text',
-									  'type' => 'text'));?></label>
+  <?php echo form_open('login/validate');?>
+  <div>
+  <label>Username<?php echo form_input(array('name' => 'username',
+										     'value' => '',
+											 'class' => 'text',
+											 'type' => 'text'));?></label>
+  </div>
+  <div>
+  <label>Password<?php echo form_password(array('name' => 'password',
+									  			'value' => '',
+												'class' => 'text',
+												'type' => 'text'));?></label>
+  </div>
   <?php echo form_submit('submit', 'Login'); ?>
+  <?php echo form_close(); ?>
 </div>
     
