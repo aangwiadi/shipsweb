0917ef16611aefdc188e0f0e349e38029a48e6ef
ghovmand <ghovmand@1fe88100-d82c-11de-a027-019ae363d039>
2009-12-07 07:56:33 +0000

    City implementation

--- system/application/models/city_model.php
+++ system/application/models/city_model.php
@@ -2,45 +2,55 @@
 
 class City_model extends Model
 {
-    function City_model()
-    {
-	parent::Model();
-	$this->load->database();
-    }
+	protected $table = "CITY";
+	protected $columns = "CITY.Id, CITY.Name, CountryId";
 
-    function create($data)
-    {
-	$this->db->insert('CITY');
-	return;
-    }
+	function City_model()
+	{
+		parent::Model();
+		$this->load->database();
+	}
 
-    function get($id)
-    {
-	$query = $this->db->get_where('CITY', array('Id' => $id));
-	if($query->num_rows() == 1)
+	function create($data)
 	{
-	    return $query->row();
+		$this->db->insert('CITY');
+		return;
 	}
-    }
-    
-    function get_all()
-    {
-	$query = $this->db->get('CITY');
-	if($query->num_rows() > 0)
+
+	function get($id)
 	{
-	  return $query->result_array();
+		$sql = "SELECT $this->columns, COUNTRY.Name AS CountryName 
+				FROM $this->table
+				LEFT JOIN COUNTRY ON CITY.CountryId = COUNTRY.Id 
+				WHERE CITY.Id = ?";
+		$query = $this->db->query($sql, array('Id' => $id));
+		if($query->num_rows() == 1)
+			return $query->row();
 	}
-    }
-    
-    function get_all_ordered()
-    {
-	$this->db->order_by('Name');
-	$query = $this->db->get('CITY');
-	if($query->num_rows() > 0)
+
+	function get_all()
 	{
-	  return $query->result_array();
+		$query = $this->db->get('CITY');
+		if($query->num_rows() > 0)
+		{
+			return $query->result_array();
+		}
 	}
-    }
+
+	function get_all_ordered()
+	{
+		$this->db->order_by('Name');
+		$query = $this->db->get('CITY');
+		if($query->num_rows() > 0)
+		{
+			return $query->result_array();
+		}
+	}
+
+	function update($id, $data)
+	{
+		$this->db->where('Id', $id);
+		return $this->db->update('CITY', $data);
+	}
 }
-
 ?>
