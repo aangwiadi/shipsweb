20d1187f0c5cc513866de870d54834c675b5c73d
ghovmand <ghovmand@1fe88100-d82c-11de-a027-019ae363d039>
2009-12-10 17:03:35 +0000

    New country

--- system/application/views/country_view.php
+++ system/application/views/country_view.php
@@ -1,7 +1,7 @@
   <header>
     <h1><?php echo $country->Name; ?></h1>
   </header> 
-  <p><?php echo anchor(site_url().'/country/edit/'.$country->Id, 'Edit').' | '.anchor('', 'New') ?></p>
+  <p><?php echo anchor(site_url().'/country/edit/'.$country->Id, 'Edit').' | '.anchor('/country/add/', 'New') ?></p>
   <ul class="section-left">
     <li><span>Name:</span><?php echo $country->Name; ?></li>
   </ul>
