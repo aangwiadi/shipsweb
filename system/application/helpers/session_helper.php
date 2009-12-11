ffee78b0dd39c6acf659c49f14bb8786a31bcb38
ghovmand <ghovmand@1fe88100-d82c-11de-a027-019ae363d039>
2009-12-11 06:55:40 +0000

    Session login page updated

--- system/application/helpers/session_helper.php
+++ system/application/helpers/session_helper.php
@@ -8,9 +8,8 @@
     $is_logged_in = $CI->session->userdata('is_logged_in');
     if(!isset($is_logged_in) || $is_logged_in != true)
     {
-      echo 'You don\'t have permission to access this page. <a href="login">Login</a>';	
-      die();		
-      //$this->load->view('login_form');
+      echo "You do not have permission to access this page " . anchor(site_url().'/login', 'Login');	
+      die();
     }
   }	
 
