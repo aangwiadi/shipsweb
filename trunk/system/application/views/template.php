6b9e03074e77a86322c4fc05352bb6bcad38a265
ghovmand <ghovmand@1fe88100-d82c-11de-a027-019ae363d039>
2010-01-10 13:57:31 +0000

    Deleted fancybox (problem with styling)

--- trunk/system/application/views/template.php
+++ trunk/system/application/views/template.php
@@ -4,9 +4,9 @@
    <title>Ships</title>
    <link href="<?php print base_url();?>css/baseline.reset.css" media="screen" rel="stylesheet" type="text/css" />
    <link href="<?php print base_url();?>css/ships.css" media="screen" rel="stylesheet" type="text/css" />
-   <link href="<?php print base_url();?>js/fancybox/jquery.fancybox-1.2.6.css" media="screen" rel="stylesheet" type="text/css" />   
+   <link href="<?php print base_url();?>js/greybox.css" media="screen" rel="stylesheet" type="text/css" />   
    <script type="text/javascript" src="<?php print base_url();?>js/jquery-1.3.2.min.js"></script> 
-   <script type="text/javascript" src="<?php print base_url();?>js/fancybox/jquery.fancybox-1.2.6.js"></script> 
+   <script type="text/javascript" src="<?php print base_url();?>js/greybox.js"></script> 
 </head>
 <body>
   <div id="page">
@@ -30,3 +30,13 @@
   </div>
 </body>
 </html>
+    <script type="text/javascript">
+      var GB_ANIMATION = true;
+      $(document).ready(function(){
+        $("a.greybox").click(function(){
+          var t = this.title || this.innerHTML || this.href;
+          GB_show(t,this.href,470,600);
+          return false;
+        });
+      });
+    </script>
