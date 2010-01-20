2a6f8b3f2b0e899e764f6c7d48c118a27df75dc7
ghovmand <ghovmand@1fe88100-d82c-11de-a027-019ae363d039>
2010-01-20 19:33:36 +0000

    changed show of errors

--- trunk/system/application/views/city_edit_view.php
+++ trunk/system/application/views/city_edit_view.php
@@ -1,12 +1,12 @@
-	<h1><?php echo $city->Name; ?></h1>
-	<?php echo validation_errors(); ?>
-    <?php echo form_open(site_url()."/city/save/$city->Id"); ?>
-    <div id="menu-button">
-      <?php echo "\t" . form_submit('save', 'Save')."\n"; ?>
-      <?php echo "\t" . anchor(site_url()."/city/index/$city->Id", "Cancel"); ?>
-    </div>
-    <ul class="section">
-      <li><?php echo form_label('Name', 'Name') . form_input('Name', $city->Name); ?></li>
-      <li><?php echo form_label('Countries', 'Countries') . form_dropdown('Countries', $ddcountry, $city->CountryId, 'class="dd-list"'); ?></li>
-    </ul>
-    <?php form_close(); ?>
+<?php echo validation_errors(); ?>
+<h1><?php echo $city->Name; ?></h1>
+<?php echo form_open(site_url()."/city/save/$city->Id"); ?>
+<div id="menu-button">
+  <?php echo "\t" . form_submit('save', 'Save')."\n"; ?>
+  <?php echo "\t" . anchor(site_url()."/city/index/$city->Id", "Cancel"); ?>
+</div>
+<ul class="section">
+  <li><?php echo form_label('Name', 'Name') . form_input('Name', $city->Name); ?></li>
+  <li><?php echo form_label('Countries', 'Countries') . form_dropdown('Countries', $ddcountry, $city->CountryId, 'class="dd-list"'); ?></li>
+</ul>
+<?php form_close(); ?>
